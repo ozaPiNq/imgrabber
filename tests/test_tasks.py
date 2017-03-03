5d63873a0bfcc09fc0eacb68a941e8423bfc04a5
Nikolay Sedelnikov <n.sedelnikov@gmail.com>
2017-03-03 14:14:51 +0300

    added get_filename test for Content-Disposition

--- tests/test_tasks.py
+++ tests/test_tasks.py
@@ -1,3 +1,5 @@
+import pytest
+
 from imgrabber import tasks
 
 
@@ -18,3 +20,22 @@ class TestFetchUrl(object):
 
         assert new_context['data'] == expected_response['text']
         assert new_context['headers'] == expected_response['headers']
+
+
+class TestGetFilename(object):
+    @pytest.mark.xfail
+    @pytest.mark.parametrize("cd, expected_filename ", [
+        ('inline',                                          'picturename.jpg'),
+        ('attachment',                                      'picturename.jpg'),
+        ('attachment; filename="pic.jpg"',                  'pic.jpg'),
+        ('form-data; name="fieldName"',                     'picturename.jpg'),
+        ('form-data; name="fieldName"; filename="pic.jpg"', 'pic.jpg'),
+    ])
+    def test_get_filename_from_content_disposition_header(self, context, cd,
+                                                          expected_filename):
+        context['headers'] = {'Content-Disposition': cd}
+        context['url'] = 'http://localhost/picturename.jpg'
+
+        new_context = tasks.get_filename()(context)
+
+        new_context['filename'] == expected_filename
