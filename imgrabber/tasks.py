13eadfb7993c5beae1f35c520ecd8972fda51bd9
Nikolay Sedelnikov <n.sedelnikov@gmail.com>
2017-03-03 14:47:20 +0300

    added docstrings to tasks

--- imgrabber/tasks.py
+++ imgrabber/tasks.py
@@ -7,6 +7,12 @@ from pipeliner import task
 
 @task(depends=['url'], provides=['headers', 'data'])
 def fetch_url(context):
+    """
+    Fetch URL using HTTP GET request
+    :param url: URL to be fetched
+
+    :return: Returns HTTP headers and HTTP payload (data)
+    """
     url = context.get('url')
 
     result = requests.get(url)
@@ -17,6 +23,13 @@ def fetch_url(context):
 
 @task(depends=['url', 'headers'], provides=['filename'])
 def get_filename(context):
+    """
+    Get filename from Content-Disposition header or URL
+    :param url: fetched file url
+    :param headers: fetched URL headers
+
+    :return: file name. If none found it will be blank
+    """
     url = context.get('url')
     headers = context.get('headers', {})
 
@@ -32,6 +45,13 @@ def get_filename(context):
         context['filename'] = parsed_url.path.split('/')[-1]
 
 
-@task(depends=['data'])
-def save_file(context):
+@task(depends=['data', 'filename'])
+def save_file(context, folder):
+    """
+    Save file to disk
+    :param folder: folder for file
+    :param filename: file name
+    :param data: data to be saved to file
+    :return:
+    """
     pass
